280e02e6bc0e2a173d8ee8547c3edc9f19080cdc
gr_wellcome <panagiotis.plytas@gmail.com>
2018-11-04 01:52:31 +0200

    Make design changes based on material design bootstrap

--- js/controllers/findTraineeCtrl.js
+++ js/controllers/findTraineeCtrl.js
@@ -5,8 +5,16 @@
 
 			$rootScope.currentPage = 'find';
 
+			$scope.goToTraineeMeasurements = function(traineeId) {
+				window.open(
+					'#!/measurements/show/' + traineeId,
+					'_blank' // <- This is what makes it open in a new window.
+				);
+			}
+
 			$scope.goToTraineeInfo = function(traineeId) {
-				window.location.href = '#!/measurements/show/' + traineeId;
+				window.location.href = '#!/trainee/show-update/' + traineeId;
+				$('[data-toggle="tooltip"]').tooltip('hide');
 			}
 
 			/* ================= On start ================= */
